576b1a149c3940f324018a917c1891b267960b58
himanshu-varshney315 <himanshu.gupta_cs.aiml24@gla.acin>
2026-02-19 15:26:35 +0530

    initialize IR sensor setup and add editor configuration

--- src/main.cpp
+++ src/main.cpp
@@ -10,35 +10,14 @@
  * and detects obstacle presence.
  */
 
- // TODO 1:
- // Define IR sensor digital pin (Use pin 2)
 
- // TODO 2:
- // Create variable to store sensor state
 
 void setup() {
 
-    // TODO 3:
-    // Initialize Serial communication (9600 baud rate)
 
-    // TODO 4:
-    // Configure IR pin as INPUT
-
-    // TODO 5:
-    // Print system initialization message
 }
 
 void loop() {
 
-    // TODO 6:
-    // Read digital value from IR sensor
 
-    // TODO 7:
-    // If obstacle detected
-    //     Print "Obstacle Detected"
-    // Else
-    //     Print "No Obstacle"
-
-    // TODO 8:
-    // Add small delay (200–500ms)
 }
