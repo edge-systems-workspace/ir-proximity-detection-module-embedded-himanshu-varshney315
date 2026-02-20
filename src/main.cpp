5b6ee97d7474b83b7822165194fa6100a6c6f7cc
himanshu-varshney315 <himanshu.gupta_cs.aiml24@gla.acin>
2026-02-20 08:29:25 +0530

    implement obstacle detection logic using IR sensor

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,31 @@
 /**
  * @file main.cpp
  * @brief Embedded Obstacle Detection System using IR Sensor
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author himanshu gupta
+ * @date 2026-02-16
  *
  * @details
  * Reads digital input from IR sensor
  * and detects obstacle presence.
  */
-
+int irPin = 7;
 
 
 void setup() {
+    pinMode(irPin, INPUT);
+    Serial.begin(9600);
 
 
 }
 
 void loop() {
+    int value = digitalRead(irPin);
+    if (value ==LOW) { //Active LOW sensor
+        Serial.println("Object Detected");
+    } else {
+        Serial.println("No Object Detected");
+    }
+    delay(300);
 
 
 }
